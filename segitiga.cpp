4282985419920d47fb8677be8c8d1aaacbd4939f
BlackSkull457 <93428091+BlackSkull457@users.noreply.github.com>
2024-10-01 00:24:54 +0700

    Update segitiga.cpp

--- segitiga.cpp
+++ segitiga.cpp
@@ -4,28 +4,25 @@
 main ()
 {
     //pendefinisian tipe variabel
-    int a, b, c;
+    int a;
     //pendefinisian variabel khusus yang akan berhubungan dengan akar
-    double d, e, f, g, h;
+    double b, c, d, e, f;
     //input variabel untuk mencari hipotenusa
     a = 5;
-    b = 3;
-    c = 2;
-    //perhitungan akar
-    d = sqrt(b);
+    b = sqrt(3)/2;
     //perhitungan dengan rumus sisi alas/cosθ
-    e = a/(d/c);
+    c = a/b;
     //menampilkann hipotenusa atau sisi miring
-    printf("sisi miring/hipotenusa = %f\n", e);
+    printf("sisi miring/hipotenusa = %f\n", c);
     //mencari sisi depan dengan rumus b^2=c^2-a^2
-    f = (e*e)-(a*a);
-    g = sqrt(f);
+    d = (c*c)-(a*a);
+    e = sqrt(d);
     //menampilkan sisi depan
-    printf("sisi depan = %f\n", g);
+    printf("sisi depan = %f\n", e);
     //mencari keliling menggunakan rumus s1+s2+s3
-    h = a+e+g;
+    f = a+c+e;
     //menampilkan keliling
-    printf("Keliling segitiga nya adalah = %f\n", h);
+    printf("Keliling segitiga nya adalah = %f\n", f);
 
     return 0;
 
